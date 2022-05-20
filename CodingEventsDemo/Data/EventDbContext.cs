0a9557599dfff1227df46ecdad608b75e39dace2
Samantha Hughes <samantha.patricia.hughes2@gmail.com>
2022-05-20 12:30:35 -0400

    DbContext

---
+++ CodingEventsDemo/Data/EventDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using CodingEventsDemo.Models;
+
+namespace CodingEventsDemo.Data
+{
+    public class EventDbContext : DbContext
+    {
+
+        public DbSet<Event> Events { get; set; }
+
+        public EventDbContext(DbContextOptions<EventDbContext> options) : base(options)
+        {
+        }
+    }
+}
